afb7e8d3c67dca92197c98f41e14f3b3cf1f32df
Ganeshwala <ganeshwalachintan@gmail.com>
2022-01-04 12:34:36 -0500

    implemented POST, PUT, and DELETE REST API

--- MessengerWebRestAPI/src/main/java/com/jersey/MessengerRest/MessengerWebRestAPI/Resources/MessageResource.java
+++ MessengerWebRestAPI/src/main/java/com/jersey/MessengerRest/MessengerWebRestAPI/Resources/MessageResource.java
@@ -2,7 +2,11 @@ package com.jersey.MessengerRest.MessengerWebRestAPI.Resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -12,7 +16,8 @@ import com.jersey.MessengerRest.MessengerWebRestAPI.Model.Messages;
 import com.jersey.MessengerRest.MessengerWebRestAPI.Services.MessageService;
 
 @Path("/messages")
-@Produces("application/xml")
+@Produces("application/json")
+@Consumes("application/json")
 public class MessageResource {
 
 	MessageService service = new MessageService();
@@ -28,4 +33,22 @@ public class MessageResource {
 	public Messages getMessage(@PathParam(value="messageID") long mId){
 		return service.getMessage(mId);
 	}
+	
+	@POST
+	public Messages addMessage(Messages msg) {
+		return service.addMessage(msg);
+	}
+	
+	@PUT
+	public Messages updateMessage(Messages msg) {
+		return service.updateMessage(msg);
+	}
+	
+	@DELETE
+	@Path("/{deleteMID}")
+	public Messages deleteMessage(@PathParam("deleteMID") long mId) {
+		return service.deleteMessage(mId);
+	}
+	
+	
 }
